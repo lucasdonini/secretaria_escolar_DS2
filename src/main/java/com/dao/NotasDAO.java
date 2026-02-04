b87086b91ab3370490a72a3d12e7ed2fe632f327
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-04 15:57:22 -0300

    Adicona método DAO para professor visualizar todas as notas publicadas por sua matéria

--- src/main/java/com/dao/NotasDAO.java
+++ src/main/java/com/dao/NotasDAO.java
@@ -3,13 +3,26 @@ package com.dao;
 import com.model.Aluno;
 import com.model.Disciplina;
 import com.model.Nota;
+import com.model.Professor;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class NotasDAO extends DAO {
     public NotasDAO() throws SQLException {
         super();
+    }
+
+    private static Nota resultSetParaNota(ResultSet rs) throws SQLException {
+        var disciplina = Disciplina.deCodigo(rs.getInt("cod_materia"));
+        return Nota.builder()
+                .n1(rs.getDouble("n1"))
+                .n2(rs.getDouble("n2"))
+                .disciplina(disciplina)
+                .build();
     }
 
     public void carregarNotas(Aluno aluno) throws SQLException {
@@ -19,13 +32,7 @@ public class NotasDAO extends DAO {
 
             try (var rs = pstmt.executeQuery()) {
                 while (rs.next()) {
-                    var disciplina = Disciplina.deCodigo(rs.getInt("cod_materia"));
-                    var nota = Nota.builder()
-                            .n1(rs.getDouble("n1"))
-                            .n2(rs.getDouble("n2"))
-                            .disciplina(disciplina)
-                            .build();
-
+                    var nota = resultSetParaNota(rs);
                     aluno.getNotas().add(nota);
                 }
             }
@@ -43,6 +50,30 @@ public class NotasDAO extends DAO {
             pstmt.setInt(4, codDisciplina);
 
             pstmt.executeUpdate();
+        }
+    }
+
+
+    public List<Nota> buscarNotasPorProfessor(Professor professor) throws SQLException {
+        var sql = """
+                SELECT n.n1, n.n2, n.cod_materia
+                FROM nota n
+                JOIN disciplina d ON d.codigo = n.cod_materia
+                WHERE n.cod_materia = ?
+                """;
+
+        var notas = new ArrayList<Nota>();
+        try (var pstmt = conn.prepareStatement(sql)) {
+            for (var disciplina : professor.getDisciplinas()) {
+                var codigo = disciplina.getCodigo();
+                pstmt.setInt(1, codigo);
+
+                try (var rs = pstmt.executeQuery()) {
+                    while (rs.next()) notas.add(resultSetParaNota(rs));
+                }
+            }
+
+            return notas.stream().distinct().toList();
         }
     }
 }
